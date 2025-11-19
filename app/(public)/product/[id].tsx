2c59c0a26a4414e3b93380c61aab9cbd3cc7563e
eromose-le <erons.a.gberaese@gmail.com>
2025-11-19 01:20:25 +0100

    chore: clean up product detail link

--- app/(public)/product/[id].tsx
+++ app/(public)/product/[id].tsx
@@ -1,15 +1,5 @@
 import ProductDetail from "@/components/product/ProductDetail";
-import { Logger } from "@/utils/logger";
-import { useLocalSearchParams } from "expo-router";
 
 export default function ProductDetailPublic() {
-  const { product } = useLocalSearchParams<{
-    id?: string;
-    product?: any[];
-  }>() as any;
-  const item = JSON.parse(product);
-
-  Logger.warn("product", item);
-
   return <ProductDetail />;
 }
