4c51809f00b6aea7fd47f43872d8e1025a8a89c5
eromose-le <erons.a.gberaese@gmail.com>
2025-11-20 18:57:03 +0100

    chore: separate the Headers v2

--- components/common/AppProtectedHeader.tsx
+++ components/common/AppProtectedHeader.tsx
@@ -1,10 +1,28 @@
-import { View, Text } from "react-native";
+import { PROFILE } from "@/constants/urls";
+import { useAuthUser } from "@/hooks/useAuthUser";
+import { router } from "expo-router";
 import React from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import Logo from "./Logo";
 
 const AppProtectedHeader = () => {
+  const { user } = useAuthUser();
+
+  const userName =
+    `${user?.firstName ?? ""} ${user?.lastName ?? ""}`.trim() || "Guest";
+
   return (
-    <View>
-      <Text>AppProtectedHeader</Text>
+    <View className="flex-row items-center justify-between mb-2">
+      <Logo />
+
+      <View className="flex-row gap-2">
+        <TouchableOpacity
+          onPress={() => router.push(PROFILE)}
+          className="items-center justify-center bg-black rounded-full w-9 h-9"
+        >
+          <Text className="font-semibold text-white">{userName[0] ?? "U"}</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
--- components/common/AppPublicHeader.tsx
+++ components/common/AppPublicHeader.tsx
@@ -1,10 +1,22 @@
-import { View, Text } from "react-native";
+import { useAuth } from "@/providers/auth";
 import React from "react";
+import { Text, View } from "react-native";
+import { BackButton } from "./BackButton";
+import Logo from "./Logo";
 
 const AppPublicHeader = () => {
+  const { unSkipLogin } = useAuth();
+
   return (
-    <View>
-      <Text>AppPublicHeader</Text>
+    <View className="flex-row items-center justify-between mb-2">
+      <Logo />
+
+      <View className="items-center">
+        <BackButton onPress={unSkipLogin} />
+        <Text className="mb-3 text-xs font-light text-secondary font-jost">
+          Sign in
+        </Text>
+      </View>
     </View>
   );
 };
